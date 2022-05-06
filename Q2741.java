a4f1ba64f5198c23b8821dadf291e6eb69da490c
kiya <kiyamoon@daum.net>
2022-05-06 23:53:05 +0900

    boj2741

---
+++ Q2741.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q2741 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		for (int i = 0; i < N; i++) {
+			bw.write(String.valueOf(i+1) + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
